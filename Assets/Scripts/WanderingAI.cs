54e0151357ef02e995e38a4ae8d1db3e3917e893
Luskander <55799709+Luskander@users.noreply.github.com>
2020-09-18 15:55:44 +0300

    Initial commit

---
+++ Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+    private bool _alive;
+
+    public float Speed = 3.0f;
+    public float ObstacleRange = 5.0f;
+
+	private void Start()
+	{
+        _alive = true;
+	}
+	void Update()
+    {
+        if(_alive)
+		{
+            transform.Translate(0, 0, Speed * Time.deltaTime);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.SphereCast(ray, 0.75f, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (_fireball == null)
+                    {
+                        _fireball = Instantiate(fireballPrefab);
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < ObstacleRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+        }
+    }
+
+    public void SetAlive(bool alive)
+	{
+        _alive = alive;
+	}
+}
